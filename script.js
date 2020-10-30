07378a14ec20bf150b88e62a40be712d2178d6d5
Cassie Jones <cassie@Cassies-MacBook-Pro.local>
2020-10-30 12:09:17 -0500

    Final files for submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+
+fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+    response.json().then(function (json) {
+    
+        json.sort(function(a,b) {
+            return b.hoursInSpace - a.hoursInSpace;
+        });
+
+        let container = document.getElementById("container");
+         for (let i = 0; i < json.length; i++) {
+            container.innerHTML += `
+            <div class="astronaut">
+                <div class="bio">
+                <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                <ul>
+                    <li>Hours in space: ${json[i].hoursInSpace}</li>
+                    <li class="active">Active: ${json[i].active}</li>
+                    <li>Skills: ${json[i].skills.join(', ')}</li>
+                </ul>
+            </div>
+            <img class="avatar" src=${json[i].picture}>
+            </div>
+            `;
+         }
+         let activeColor = document.getElementsByClassName("active");
+         for (let j = 0; j < activeColor.length; j++) {
+             if (json[j].active === true) {
+                activeColor[j].style.color="green"; 
+             }
+         }
+
+        });
+    });
+});
